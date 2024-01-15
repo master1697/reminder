ec1b68d11021f0e56a28b57f6d4e29f6e8443201
Pal Makkar <pmakkar97@gmail.com>
2024-01-14 19:57:01 -0500

    add yaml and tasks.py

---
+++ tasks.py
@@ -0,0 +1,22 @@
+import os
+from celery import Celery
+from celery.utils.log import get_task_logger
+
+app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
+logger = get_task_logger(__name__)
+
+EMAIL = "remindmenotif@gmail.com"
+PASSWORD = "xkzgnnyogyoakduo"
+
+@app.task
+def send_sms(recipient, message):
+    logger.info(f'Adding {recipient} + {message}')
+    # SMTP setup and message creation here...
+    # ...
+    print("celery task!", file=sys.stderr)
+    auth = (EMAIL, PASSWORD)
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(auth[0], auth[1])
+    server.sendmail(auth[0], recipient, message)
+    server.quit()
